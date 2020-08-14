bb49dc54ceeea79c8235feb1a52410f1e3bb539a
rtoddf <rtoddf@me.com>
2020-08-14 11:51:15 -0400

    selections and data joins

--- src/Components/D3Chart.js
+++ src/Components/D3Chart.js
@@ -1,18 +1,27 @@
 import * as d3 from "d3";
 
+const data = [20, 12, 16, 25, 20],
+    width = 500,
+    height = 300;
+
 export default class D3Chart {
     constructor(element) {
         const svg = d3.select(element)
             .append("svg")
-                .attr("width", 500)
-                .attr("height", 500)
+                .attr("width", width)
+                .attr("height", height)
 
-        svg.append("rect")
-            .attr("x", 50)
-            .attr("y", 50)
-            .attr("width", 100)
-            .attr("height", 400)
-            .attr("fill", "gray")
+        const rects = svg.selectAll("rect")
+            .data(data)
+
+        rects.enter()
+            .append("rect")
+                .attr("x", (d, i) => i * 100)
+                .attr("y", (d, i) => height - d)
+                .attr("width", 50)
+                .attr("height", d => d)
+                .attr("fill", "gray")
+            
         // console.log(d3.select(element))
     }
 }
